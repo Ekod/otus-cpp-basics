16aafa1d28a4d8f4a84a9c65a6730cc143f4c14a
Gleb Akishin <ekod87@yandex.ru>
2022-11-02 12:44:47 +0300

    done

---
+++ hw_4/include/variable.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "astnode.hpp"
+
+class Variable : public ASTNode {
+public:
+    Variable(std::string val)
+            : ASTNode(val)
+            , val_(val) {}
+
+    std::string value() const { return val_; }
+
+private:
+    std::string val_;
+};
