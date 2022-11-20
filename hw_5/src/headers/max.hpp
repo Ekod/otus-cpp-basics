e7940212d7ff0a09590e485a2219f3d9e70cd899
Gleb Akishin <ekod87@yandex.ru>
2022-11-20 17:22:22 +0300

    fix

--- hw_5/src/headers/max.hpp
+++ hw_5/src/headers/max.hpp
@@ -3,7 +3,7 @@
 
 class Max : public IStatistics {
 public:
-    Max() : m_max{std::numeric_limits<double>::max()} {}
+    Max() : m_max{std::numeric_limits<double>::min()} {}
 
     void update(double next) override {
         if (next > m_max) {
@@ -15,7 +15,7 @@ public:
         return m_max;
     }
 
-    const char * name() const override {
+    const char* name() const override {
         return "max";
     }
 
