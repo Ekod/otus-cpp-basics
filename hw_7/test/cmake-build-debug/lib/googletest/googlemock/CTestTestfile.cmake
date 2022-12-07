ea7066d2cb2253991c2f0289131be8d27278ae0d
Gleb Akishin <ekod87@yandex.ru>
2022-12-07 13:52:25 +0300

    fix

--- hw_7/test/cmake-build-debug/lib/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Ekod/Documents/prog/cpp/otus-cpp-basics/hw_7/test/lib/googletest/googlemock
-# Build directory: C:/Users/Ekod/Documents/prog/cpp/otus-cpp-basics/hw_7/test/cmake-build-debug/lib/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
