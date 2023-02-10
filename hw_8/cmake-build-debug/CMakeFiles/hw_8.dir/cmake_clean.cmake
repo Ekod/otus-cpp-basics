94bcf67a9beacfbc656e766466dbdec6655cabf7
G A <ekod87@yandex.ru>
2023-02-10 15:02:24 +0300

    fix

---
+++ hw_8/cmake-build-debug/CMakeFiles/hw_8.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw_8.dir/main.cpp.obj"
+  "CMakeFiles/hw_8.dir/main.cpp.obj.d"
+  "hw_8.exe"
+  "hw_8.exe.manifest"
+  "hw_8.pdb"
+  "libhw_8.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw_8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
